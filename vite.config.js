dcd707f8c59b9e70c3e4a03dc07d127041e7cff9
shallymysore <bkshashankhr@gmail.com>
2025-03-23 23:23:04 -0700

    push code changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/efl2025_first/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
